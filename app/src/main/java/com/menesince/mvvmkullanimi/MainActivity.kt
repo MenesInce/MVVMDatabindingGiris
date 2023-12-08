f4f6909e14e4c53952f2983dc0efd22e6b8e3a79
MENEŞ <inceem01@gmail.com>
2023-12-08 13:02:25 +0300

    Databinding

---
+++ app/src/main/java/com/menesince/mvvmkullanimi/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.menesince.mvvmkullanimi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.menesince.mvvmkullanimi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.mainActivityNesnesi = this
+        binding.hesaplamaSonucu = "0"
+
+
+    }
+    fun buttonToplamaTikla (alinanSayi1 :String, alinanSayi2: String){
+        val sayi1 = alinanSayi1.toInt()
+        val sayi2 = alinanSayi2.toInt()
+
+        val toplam = sayi1 + sayi2
+        binding.hesaplamaSonucu  = toplam.toString()
+    }
+    fun buttonCarpmaTikla (alinanSayi1 :String, alinanSayi2: String){
+
+        val sayi1 = alinanSayi1.toInt()
+        val sayi2 = alinanSayi2.toInt()
+
+        val carp = sayi1 * sayi2
+        binding.hesaplamaSonucu = carp.toString()
+    }
+}
